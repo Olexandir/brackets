4bb1c4c081aaf6694d8c917701fe33dc4b3485a5
XV-22 <68996102+Olexandir@users.noreply.github.com>
2021-02-24 18:04:59 +0300

    feat: refactor function
    refactor function

--- src/index.js
+++ src/index.js
@@ -1,33 +1,20 @@
 // module.exports = function check(str, bracketsConfig) {
-    module.exports = function check(s) {
-    var pairs = {
-        '}':'{',
-        ']':'[',
-        ')':'(',
-    };
-    var stack = [];
-    for(var i = 0;i < s.length;++i){
-        switch(s.charAt(i)){
-            case '[': case '{':case '(':
-                stack.push(s.charAt(i));
-                break;
-            case ']': case '}':case ')':
-                if(isStackEmpty(stack) || peek(stack) !== pairs[s.charAt(i)]) return false;
-                stack.pop();
-                break;
-            case '"':
-                if(isStackEmpty(stack) || peek(stack) !== s.charAt(i)){
-                    stack.push(s.charAt(i));
-                }else{
-                    stack.pop();
-                }
-        }
+    module.exports = function check(str, bracketsConfig) {
+  var stack = [];
+  var existBracket = {};
+  for(var j = 0; j < bracketsConfig.length; j += 1) {
+    existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+  }
+
+  stack.push(str.charAt(0));
+
+  for(var i = 1; i < str.length; i += 1) {
+    if(str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+      stack.pop();
+    } else {
+      stack.push(str.charAt(i)); 
     }
-    return isStackEmpty(stack);
-}
-function isStackEmpty(s){
-    return s.length === 0;
-}
-function peek(s){
-    return s[s.length-1];
+  }
+  if (stack.length > 0) {return false;}
+  return true;
 }
